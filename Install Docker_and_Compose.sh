f6091381f1d64fcc63eeb684b1b2d0e276168d6f
PirateTM <40763772+piratetm@users.noreply.github.com>
2020-07-15 16:49:55 +0300

    Update Install Docker_and_Compose.sh
    Добавил отдельный docker.list

--- Install Docker_and_Compose.sh
+++ Install Docker_and_Compose.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
- 
-echo "deb [trusted=yes arch=amd64] https://download.docker.com/linux/debian stretch stable" >> /etc/apt/sources.list
 apt update
- 
 apt install -y
     libicu57 \
     apt-transport-https \
@@ -10,10 +7,16 @@ apt install -y
     curl \
     gnupg-agent \
     software-properties-common
+ 
+echo "deb [trusted=yes arch=amd64] https://download.docker.com/linux/debian stretch stable" > /etc/apt/sources.list.d/docker.list
+apt update
+ 
+
 apt install -y docker-ce docker-ce-cli containerd.io
+
+docker --version
  
- 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
